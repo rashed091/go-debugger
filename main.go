e0e2313d11a6a3b7a19e05552511a2b0fbdfcb39
mrasheduzzaman <rashed091nsu@gmail.com>
2023-04-28 21:45:10 +0600

    Update go app

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 func main() {
     r := chi.NewRouter()
     r.Use(middleware.Logger)
-    r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+    r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
         w.Write([]byte("Hello World!"))
     })
     http.ListenAndServe(":3573", r)
